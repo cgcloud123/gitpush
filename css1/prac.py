4c13b1f5e077efb9bf907629c2164c63f795aaec
Suman Parimal <suman.parimal@cinegencemedia.com>
2022-10-12 16:35:51 +0530

    Added my project

---
+++ css1/prac.py
@@ -0,0 +1,10 @@
+list1 = [2,3,4,5,6,7]
+list2 =[]
+
+for number in range(len(list1)):
+    sq_number = list1[number]**2
+    list2.append(sq_number)
+
+# print(list2)
+# print(list1)
+print(list2)
---
+++ css1/style2.cs
@@ -0,0 +1,31 @@
+    
+   #career2{
+    height: 100%;
+   }
+   #career3{
+    height:900px;
+   }
+   .careimg{
+    width: 350px;
+    height: 400px;
+    position: relative;
+    top: 4rem;
+   }
+   .img2 img{
+    display: none;
+   }
+   .career2 h2{
+    font-size: 30px;
+   }
+   .career_head h1{
+    font-size: 35px;
+    position: relative;
+    left:-2rem;
+   }
+   .newcarpara{
+    position: relative;
+    top: 2rem;
+   }
+   #career{
+    background-position: center;
+   }
